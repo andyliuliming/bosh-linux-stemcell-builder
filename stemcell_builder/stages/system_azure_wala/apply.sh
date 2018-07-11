71f054791b36457032dbc8ad3e59e791b4090c2e
Andy Liu <andyliuliming@outlook.com>
2018-07-11 16:39:40 +0800

    use the report early.

--- stemcell_builder/stages/system_azure_wala/apply.sh
+++ stemcell_builder/stages/system_azure_wala/apply.sh
@@ -8,10 +8,10 @@ source $base_dir/lib/prelude_apply.bash
 packages="python python-pyasn1 python-setuptools"
 pkg_mgr install $packages
 
-wala_release=2.2.25
-wala_expected_sha1=8fb9ef0558c11b70b48188fb5afd53eadc321fac
+# wala_release=2.2.25
+wala_expected_sha1=e7c7b095e69c40c2d92a82d64f17654128b8a63e
 
-curl -L https://github.com/Azure/WALinuxAgent/archive/v${wala_release}.tar.gz > /tmp/wala.tar.gz
+curl -L https://opensourcerelease.blob.core.windows.net/internalreleases/WALinuxAgent-2.2.27-report-early.tar.gz > /tmp/wala.tar.gz
 sha1=$(cat /tmp/wala.tar.gz | openssl dgst -sha1  | awk 'BEGIN {FS="="}; {gsub(/ /,"",$2); print $2}')
 if [ "${sha1}" != "${wala_expected_sha1}" ]; then
   echo "SHA1 of downloaded v${wala_release}.tar.gz ${sha1} does not match expected SHA1 ${wala_expected_sha1}."
@@ -24,12 +24,12 @@ mv -f /tmp/wala.tar.gz $chroot/tmp/wala.tar.gz
 run_in_chroot $chroot "
   cd /tmp/
   tar zxvf wala.tar.gz
-  cd WALinuxAgent-${wala_release}
+  cd WALinuxAgent
   sudo python setup.py install --skip-data-files
   cp bin/waagent* /usr/sbin/
   chmod 0755 /usr/sbin/waagent*
   cd /tmp/
-  sudo rm -fr WALinuxAgent-${wala_release}
+  sudo rm -fr WALinuxAgent
   rm wala.tar.gz
 "
 cp -f $dir/assets/etc/waagent.conf $chroot/etc/waagent.conf
